a3d247b04e3aae9201c07aeac3f6a03eeec5f429
HGWells07 <carlitose07@gmail.com>
2018-01-23 16:59:44 -0600

    ejemplo de cambio en schema de estado

--- src/schemas/estado.js
+++ src/schemas/estado.js
@@ -1,11 +1,32 @@
+import mongoose from 'mongoose';
 const graphql = require('graphql');
-const { GraphQLObjectType,  GraphQLString,  GraphQLID } = graphql;
+const { GraphQLObjectType,  GraphQLString,  GraphQLID, GraphQLList } = graphql;
+
+//Models
+const Estado = mongoose.model('estado');
+
+//Schemas
+const PoliticoType = require('./politico');
 
 const EstadoType = new GraphQLObjectType({
   name:  'EstadoType',
   fields: () => ({
     id: { type: GraphQLID },
-    estado: { type: GraphQLString }
+    nombre: { type: GraphQLString },
+    funcionarios: {
+      type: GraphQLList(PoliticoType),
+      resolve(parentValue) {
+        return Estado.findById(parentValue).populate('politico')
+            .then(estado => estado.funcionario);
+      }
+    },
+    candidatos: {
+      type: GraphQLList(PoliticoType),
+      resolve(parentValue) {
+        return Estado.findById(parentValue).populate('politico')
+            .then(estado => estado.candidato);
+      }
+    }
   })
 });
 

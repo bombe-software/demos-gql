666f092e1f4e18d12dc9aab3f242c3fc0e1f5d19
Vicroni <drasa_tec@hotmail.com>
2018-02-02 00:29:16 -0600

    Likes con un solo error :,(

--- src/mutations/manageLike.js
+++ src/mutations/manageLike.js
@@ -11,10 +11,16 @@ function dislikePropuesta({ args, req }) {
 
 
     return Propuesta.findById(id_propuesta)
-        .then(propuesta => {
-            propuesta.likes.push(id_usuario);
-            return Promise.resolve(propuesta.save());
+    .then(propuesta => {
+
+        //Un pequeño problema en el remove :,V
+        const array = _.remove(propuesta.likes, function (n) {
+            return n!=id_usuario;
         });
+
+        propuesta.set({likes: array});
+        return Promise.resolve(propuesta.save());
+    });
 }
 
 function likePropuesta({ args, req }) {
@@ -26,15 +32,10 @@ function likePropuesta({ args, req }) {
 
 
     return Propuesta.findById(id_propuesta)
-        .then(propuesta => {
-            const array = propuesta.likes;
-            _.remove(array, function (n) {
-                console.log()
-                return n==id_usuario;
-            });
-            propuesta.set({likes: array});
-            return Promise.resolve(propuesta.save());
-        });
+    .then(propuesta => {
+        propuesta.likes.push(id_usuario);
+        return Promise.resolve(propuesta.save());
+    });
 }
 
 module.exports = {

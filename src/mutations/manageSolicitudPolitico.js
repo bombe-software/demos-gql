1fe8d2fb032d0a5cd2f90b2d9765b188aee9aba5
Vicroni <drasa_tec@hotmail.com>
2018-02-26 23:18:32 -0600

    Correcciones leves x2

--- src/mutations/manageSolicitudPolitico.js
+++ src/mutations/manageSolicitudPolitico.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-//Importar models
-const mongoose = require('mongoose');
-const SolicitudPolitico = mongoose.model('solicitud_politico');
-const Politico = mongoose.model('politico');
-const Usuario = mongoose.model('usuario');
-
-function aceptarSolicitudPolitico({args, req}) {
-    const { id_politico } = args;
-     if (!id_politico) {
-        throw new Error('Error al hacer fetch con el Politico');
-    }
-    SolicitudPolitico.findById(id_politico)
-    .then((politico) => {
-        const { nombre, cargo, estado, partido, estudios,referencia, _id } = politico;
-
-        politicoAprovado = new Politico({
-            nombre, cargo, estado, partido, estudios, referencia
-        });
-
-        politicoAprovado.save(function (err, resp) {
-            if (err) return console.error(err);
-            SolicitudPolitico.findByIdAndRemove(_id, (err)=>{
-                if (err) return console.error(err);
-            });
-            console.log(resp._id);
-            return Politico.findById(resp._id);
-        });
-    });
-
-}
-
-function denegarSolicitudPolitico({args, req}) {
-    const { id_politico, /*id_usuario*/ } = args;
-
-    SolicitudPolitico.findByIdAndRemove(id_politico, (err)=> {
-        if(err) return console.error(err);
-    });
-
-}
-
-module.exports = { 
-    aceptarSolicitudPolitico, 
-    denegarSolicitudPolitico 
-=======
 //Importar models
 const mongoose = require('mongoose');
 const SolicitudPolitico = mongoose.model('solicitud_politico');
@@ -110,5 +65,4 @@ function denegarSolicitudPolitico({args, req}) {
 module.exports = { 
     aceptarSolicitudPolitico, 
     denegarSolicitudPolitico 
->>>>>>> origin/hgwells07
 };
--- src/schemas/politico.js
+++ src/schemas/politico.js
@@ -1,68 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLString, GraphQLList } = graphql;
-
-//importar modelos
-const Politico = mongoose.model('politico');
-
-const PoliticoType = new GraphQLObjectType({
-    name: 'PoliticoType',
-    fields: () => ({
-        id: { type: GraphQLID },
-        nombre: { type: GraphQLString },
-        cargo: { type: GraphQLString },
-        usuario: {
-            type: require('./usuario'),
-            resolve(parentValue) {
-                return Politico.findById(parentValue)
-                .populate('usuario')
-                .then(politico => politico.usuario);
-            }
-        },
-        estado: {
-            type: require('./estado'),
-            resolve(parentValue) {
-                return Politico.findById(parentValue.id)
-                    .populate('estado')
-                    .then(politico => politico.estado)
-            }
-         },
-        partido: { 
-            type: require('./partido'),
-            resolve(parentValue) {
-                return Politico.findById(parentValue.id)
-                    .populate('partido')
-                    .then(politico => politico.partido)
-            }
-         },
-        eventos: {
-            type: new GraphQLList(require('./evento')),
-            resolve(parentValue) {
-                return Politico.findById(parentValue.id)
-                    .populate('eventos')
-                    .then(politico => politico.eventos);
-            }
-        },
-        estudios: {
-            type: new GraphQLList(require('./estudio')),
-            resolve(parentValue) { 
-                return Politico.findById(parentValue.id).populate('estudios')
-                    .then(politico => politico.estudios);
-            }
-        },
-        propuestas: {
-            type: new GraphQLList(require('./propuesta')),
-            resolve(parentValue) {
-                return Politico.findById(parentValue.id)
-                    .populate('propuestas')
-                    .then(politico => politico.propuestas);
-            }
-        }
-    })
-});
-
-=======
 const mongoose = require('mongoose');
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLString, GraphQLList } = graphql;
@@ -118,5 +53,4 @@ const PoliticoType = new GraphQLObjectType({
     })
 });
 
->>>>>>> origin/hgwells07
 module.exports = PoliticoType;

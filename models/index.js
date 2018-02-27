9c8845056995aff69f0cb5885099340c26e8f85a
Vicroni <drasa_tec@hotmail.com>
2018-02-26 23:26:16 -0600

    Correcion minima xD x2

--- models/index.js
+++ models/index.js
@@ -9,6 +9,7 @@ require('./tipo_usuario');
 require('./propuesta');//requiere [tipo_propuesta]
 require('./politico'); //requiere [partido, estado, [eventos], [estudios], [propuestas]
 require('./usuario');//requiere [tipo_usuario]
+require('./logs');
 require('./usuario_confirmar');
 require('./preferencia');//requiere [politico, [usuarios]]
 require('./votacion');//requiere [estado, [preferencias]]

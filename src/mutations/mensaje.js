046da61aa0301b5f8eb53b4e0ba561f1c54e3355
Vicroni <drasa_tec@hotmail.com>
2018-03-04 15:30:11 -0600

    Creo que ya quedo el bot

--- src/mutations/mensaje.js
+++ src/mutations/mensaje.js
@@ -9,9 +9,32 @@ function mensaje({ args, req }) {
 
     return client.message(mensajeUser, {})
     .then((data) => {
-        console.log(data.entities.entities);
         if(JSON.stringify(data.entities) != '{}'){
-            return  data.entities.datos[0].value;
+            const bombe = data.entities['bombe'];
+            const humor = data.entities['humor'];
+            const datos = data.entities['datos'];
+            const politicos = data.entities['politicos'];
+            const conversacion = data.entities['conversacion'];
+            const registro = data.entities['registro'];
+            const incompatibles = data.entities['incompatibles'];
+            const tendenciasPoliticas = data.entities['tendenciasPoliticas'];
+            if(bombe){
+                return  data.entities.bombe[0].value;
+            }else if(humor){
+                return  data.entities.humor[0].value;
+            }else if(datos){
+                return  data.entities.datos[0].value;
+            }else if(politicos){
+                return  data.entities.politicos[0].value;
+            }else if(conversacion){
+                return  data.entities.conversacion[0].value;
+            }else if(registro){
+                return  data.entities.registro[0].value;
+            }else if(incompatibles){
+                return  data.entities.incompatibles[0].value;
+            }else if(tendenciasPoliticas){
+                return  data.entities.tendenciasPoliticas[0].value;
+            }
         }
         return "No logro entenderte";
     })

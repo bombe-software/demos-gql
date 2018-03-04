12f608dfd249d5ef591f1abebd0e8668f1fc00cc
Vicroni <drasa_tec@hotmail.com>
2018-03-04 14:50:18 -0600

    Commit de Deploy

--- index.js
+++ index.js
@@ -32,15 +32,15 @@ const config = {
   user: 'admin',
   password: 'n0m3l0',
 }
-const MONGO_URI = `mongodb://localhost/demos_db`;
-//const MONGO_URI = `mongodb://${config.user}:${config.password}@ds255767.mlab.com:55767/demos_db`;
+//const MONGO_URI = `mongodb://localhost/demos_db`;
+const MONGO_URI = `mongodb://${config.user}:${config.password}@ds255767.mlab.com:55767/demos_db`;
 mongoose.Promise = require('bluebird');
 mongoose.connect(MONGO_URI).catch(err => console.error(err));
 
 
 //Configruacion del CORS
 const corsOptions = {
-    origin: 'http://localhost:9000',
+    origin: 'https://demos-web.herokuapp.com/',
     credentials: true,
 }
 app.use(cors(corsOptions));

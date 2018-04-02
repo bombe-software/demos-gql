d756b6ad8ff566141e0724fc64e14978921087b3
MedinaVilla <diletalvezalaautopista@gmail.com>
2018-04-02 11:31:33 -0500

    Validar

--- src/mutations/recoverPassword.js
+++ src/mutations/recoverPassword.js
@@ -6,16 +6,21 @@ const axios = require('axios');
 
 //Funcion
 function recoverPassword({ args, req }) {
-
     let {
         email
     } = args;
-    console.log(email);
+    if(!email){
+        throw new Error('Escriba el email');
+    }
     Usuario.findOne({email})
         .then((usuario) => {
             console.log(usuario);
+            if(usuario!=null){
             const { password} = usuario;
             axios.post("http://localhost:5000/recover_password", {email,password});
+            } else {
+                throw new Error('Correo no registrado');
+            }
         });
     return "Todo Bien";
 }

247ffa1de94b11a6ea9343006a27da9a6fea2917
vicroni <drasa_tec@hotmail.com>
2018-04-26 23:38:07 -0500

    Cambios de deploy

--- deploy.js
+++ deploy.js
@@ -3,6 +3,7 @@
 /**
  * Variables de deploy
  *  
+ */
 
 exports.demos_web_http = 'https://www.demos-web.com';
 exports.demos_krb_http = "https://demos-krb.herokuapp.com";
@@ -10,15 +11,16 @@ exports.demos_gql_http = "https://demos-gql.herokuapp.com";
 exports.demos_gql_ws = "wss://demos-gql.herokuapp.com";
 exports.deploy = true;
 exports.MONGO_URI = `mongodb://admin:n0m3l0@ds255767.mlab.com:55767/demos_db`;
- */
+
 
 
 /**
  * Variables de desarrollo
- */
+
 exports.demos_web_http = "http://localhost:9000";
 exports.demos_krb_http = "http://localhost:5000";
 exports.demos_gql_http = "http://localhost:3000";
 exports.demos_gql_ws = "ws://localhost:3000";
 exports.deploy = true;
 exports.MONGO_URI = `mongodb://localhost/demos_db`;
+ */
